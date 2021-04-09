6309a4c112ed493f3db7303bb592aad0fd7c40e9
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-08 23:19:26 -0300

    add the add test

--- Tdd/MYTDDTEST/tdd/core/tests.py
+++ Tdd/MYTDDTEST/tdd/core/tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .calc import add
 from django.test import TestCase
 
 # Create your tests here.
@@ -8,7 +8,5 @@ from django.test import TestCase
 
 class CalcTest(TestCase):
     def test_add(self):
-      '''
-        Just a simple unity test
-      '''
-        self.assertEqual(add(2, 10), 10)
+        """Just a simple unity test"""
+        self.assertEqual(add(2, 10), 12)
