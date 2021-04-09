b58cc46221efa0a847d68cc1887d4f19f9d19ba8
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-08 23:13:18 -0300

    commit

--- Tdd/MYTDDTEST/tdd/core/tests.py
+++ Tdd/MYTDDTEST/tdd/core/tests.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class CalcTest(TestCase):
+    def test_add(self):
+      '''
+        Just a simple unity test
+      '''
+        self.assertEqual(add(2, 10), 10)
