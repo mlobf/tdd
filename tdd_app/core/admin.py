ba0c7ccfd07e1fedaae8d5cf8baf29860481bb33
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-12 22:43:58 -0300

    Connect to Admin from Django

--- tdd_app/core/admin.py
+++ tdd_app/core/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "name"]
 
 
-# admin.site.register()
+admin.site.register(models.User, UserAdmin)
